8a192ab0204f6289f54f7f85d8e814c6317d5c2a
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-05-15 20:54:13 +0900

    commit

--- src/main/java/com/anything/s3/S3Application.java
+++ src/main/java/com/anything/s3/S3Application.java
@@ -2,8 +2,20 @@ package com.anything.s3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableCaching
+@EnableConfigurationProperties
+@ConfigurationPropertiesScan
+@EnableJpaAuditing
+@EnableJpaRepositories
 public class S3Application {
 
     public static void main(String[] args) {
