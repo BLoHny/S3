e2c0d07a8b652b68ef02420798cc2a05ffe9a2b1
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-05-15 20:54:41 +0900

    Member

---
+++ src/main/java/com/anything/s3/domain/member/entity/Member.java
@@ -0,0 +1,30 @@
+package com.anything.s3.domain.member.entity;
+
+import com.anything.s3.domain.member.entity.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Builder
+@Table(name = "S3Member")
+@AllArgsConstructor
+public class Member {
+
+    @Id
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
