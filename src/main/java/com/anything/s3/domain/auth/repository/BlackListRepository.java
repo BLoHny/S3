069899171985f7da6d9f4337256c5150dcc8a34b
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-05-16 18:00:13 +0900

    LogOut

--- src/main/java/com/anything/s3/domain/auth/repository/BlackListRepository.java
+++ src/main/java/com/anything/s3/domain/auth/repository/BlackListRepository.java
@@ -2,6 +2,7 @@ package com.anything.s3.domain.auth.repository;
 
 import com.anything.s3.domain.auth.entity.BlackList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface BlackListRepository extends JpaRepository<BlackList, String> {
+public interface BlackListRepository extends CrudRepository<BlackList, String> {
 }
