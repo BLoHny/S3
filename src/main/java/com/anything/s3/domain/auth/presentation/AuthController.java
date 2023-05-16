975954681e0177fc0570500ea5004d1f5a3670ce
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-05-16 20:13:10 +0900

    logout, reIssueToken

--- src/main/java/com/anything/s3/domain/auth/presentation/AuthController.java
+++ src/main/java/com/anything/s3/domain/auth/presentation/AuthController.java
@@ -2,9 +2,12 @@ package com.anything.s3.domain.auth.presentation;
 
 import com.anything.s3.domain.auth.presentation.dto.request.SignInRequest;
 import com.anything.s3.domain.auth.presentation.dto.request.SignUpRequest;
+import com.anything.s3.domain.auth.presentation.dto.response.NewTokenResponse;
 import com.anything.s3.domain.auth.presentation.dto.response.SignInResponse;
 import com.anything.s3.domain.auth.service.MemberLoginService;
+import com.anything.s3.domain.auth.service.MemberLogoutService;
 import com.anything.s3.domain.auth.service.MemberSignUpService;
+import com.anything.s3.domain.auth.service.NewTokenService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,6 +21,8 @@ public class AuthController {
 
         private final MemberSignUpService signUpService;
         private final MemberLoginService loginService;
+        private final MemberLogoutService logoutService;
+        private final NewTokenService newTokenService;
 
         @GetMapping("/signup")
         public ResponseEntity<?> signUp(@Valid @RequestBody SignUpRequest request) {
@@ -29,5 +34,17 @@ public class AuthController {
         public ResponseEntity<SignInResponse> login(@Valid @RequestBody SignInRequest request) {
             SignInResponse response = loginService.execute(request);
             return new ResponseEntity<>(response, HttpStatus.OK);
+        }
+
+        @DeleteMapping
+        public ResponseEntity<?> logout(@RequestHeader("Authorization")String accessToken) {
+            logoutService.execute(accessToken);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        @PatchMapping
+        public ResponseEntity<NewTokenResponse> reIssueToken(@RequestHeader("RefreshToken") String token) {
+            NewTokenResponse newTokenResponse = newTokenService.execute(token);
+            return new ResponseEntity<>(newTokenResponse, HttpStatus.OK);
         }
 }
