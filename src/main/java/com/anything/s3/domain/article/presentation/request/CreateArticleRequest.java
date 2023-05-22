94cd7cf85ce2fbd0843497e12cc0a46047ba75de
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-05-22 19:59:56 +0900

    CreateArticle

--- src/main/java/com/anything/s3/domain/article/presentation/request/CreateArticleRequest.java
+++ src/main/java/com/anything/s3/domain/article/presentation/request/CreateArticleRequest.java
@@ -1,11 +1,14 @@
 package com.anything.s3.domain.article.presentation.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 
 public class CreateArticleRequest {
 
+    @NotBlank(message = "제목은 필수 요소입니다.")
     private String title;
+
     private String content;
 }
