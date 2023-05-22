bc38f2735e0b6207e55a9e4f806e612cd361887f
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-05-22 20:51:00 +0900

    List

--- src/main/java/com/anything/s3/domain/article/presentation/UserArticleController.java
+++ src/main/java/com/anything/s3/domain/article/presentation/UserArticleController.java
@@ -1,7 +1,9 @@
 package com.anything.s3.domain.article.presentation;
 
 import com.anything.s3.domain.article.presentation.request.CreateArticleRequest;
+import com.anything.s3.domain.article.presentation.response.ListArticleResponse;
 import com.anything.s3.domain.article.service.CreateArticleService;
+import com.anything.s3.domain.article.service.ListArticleService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,9 +20,17 @@ public class UserArticleController {
 
     private final CreateArticleService createArticleService;
 
+    private final ListArticleService listArticleService;
+
     @PostMapping
     public ResponseEntity<?> createArticle(@RequestBody @Valid CreateArticleRequest articleRequest, @RequestPart("file") List<MultipartFile> files) {
         createArticleService.execute(articleRequest, files);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<ListArticleResponse> boardList() {
+        var list = listArticleService.execute();
+        return new ResponseEntity<>(list, HttpStatus.OK);
     }
 }
