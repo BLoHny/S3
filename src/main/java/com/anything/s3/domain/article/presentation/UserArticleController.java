596e687d2f4c614b7ed7072165c65a0ca23d3c1d
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-05-22 20:00:23 +0900

    File 추가

--- src/main/java/com/anything/s3/domain/article/presentation/UserArticleController.java
+++ src/main/java/com/anything/s3/domain/article/presentation/UserArticleController.java
@@ -2,24 +2,25 @@ package com.anything.s3.domain.article.presentation;
 
 import com.anything.s3.domain.article.presentation.request.CreateArticleRequest;
 import com.anything.s3.domain.article.service.CreateArticleService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/article")
-public class ArticleController {
+@RequestMapping("/user/article")
+public class UserArticleController {
 
     private final CreateArticleService createArticleService;
 
     @PostMapping
-    public ResponseEntity<?> createArticle(@RequestBody CreateArticleRequest articleRequest) {
-        createArticleService.execute(articleRequest);
+    public ResponseEntity<?> createArticle(@RequestBody @Valid CreateArticleRequest articleRequest, @RequestPart("file") List<MultipartFile> files) {
+        createArticleService.execute(articleRequest, files);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
