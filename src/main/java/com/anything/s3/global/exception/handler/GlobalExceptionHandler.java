0d865cbe792c3ca992033a3ab2660ba527e879c5
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-06-13 17:22:14 +0900

    어노테이션 추가

--- src/main/java/com/anything/s3/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/anything/s3/global/exception/handler/GlobalExceptionHandler.java
@@ -5,12 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
+@Component
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(S3Exception.class)
